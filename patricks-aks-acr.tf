02ae3fd1f10be97e42283799520619f706aace0a
Patrick Koch <patkoch87@gmail.com>
2022-04-09 22:36:08 +0200

    Terraform configuration file for provisioning an Azure Kubernetes Service and an Azure Container Registry

---
+++ patricks-aks-acr.tf
@@ -0,0 +1,62 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 2.65"
+    }
+
+    random = {
+      source  = "hashicorp/random"
+      version = "3.1.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "patricks-rg" {
+  name     = "patricks_blog_acr_aks_rg"
+  location = "Germany West Central"
+}
+
+resource "azurerm_container_registry" "patricks-acr" {
+  name                = "patricksblog"
+  sku                 = "Premium"
+  resource_group_name = azurerm_resource_group.patricks-rg.name
+  location            = azurerm_resource_group.patricks-rg.location
+}
+
+resource "azurerm_kubernetes_cluster" "patricks-k8s-cluster" {
+  name                = "patricks-aks"
+  location            = azurerm_resource_group.patricks-rg.location
+  resource_group_name = azurerm_resource_group.patricks-rg.name
+  dns_prefix          = "patricks-dns"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_D2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  tags = {
+    Environment = "Production"
+  }
+}
+
+resource "azurerm_role_assignment" "enablePulling" {
+  principal_id                     = azurerm_kubernetes_cluster.patricks-k8s-cluster.kubelet_identity[0].object_id
+  role_definition_name             = "AcrPull"
+  scope                            = azurerm_container_registry.patricks-acr.id
+  skip_service_principal_aad_check = true
+}
+
+# Sources
+# https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/kubernetes_cluster
+# https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/container_registry
+# https://docs.microsoft.com/en-us/azure/container-registry/container-registry-skus
